e5e24848e5b9f9109d73792dcfacde9712ec85c6
leqdung <dzung.dg@gmail.com>
2023-05-23 06:17:39 +0700

    database design

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 # Technology
 - dotnet core 3.1
 - Entity framework core 3.1
+# tool
+- EF core Tool
+- EF core Design
+- EF core sqlserver
 # youtube tutorial
 # How to configure and run
 # How to contribute
---
+++ eShopSolution.Data/Migrations/20230522231541_ini.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class ini : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2023, 5, 23, 6, 15, 41, 29, DateTimeKind.Local).AddTicks(2560),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 1, 29, 21, 20, 57, 232, DateTimeKind.Local).AddTicks(9768));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 1, 29, 21, 20, 57, 232, DateTimeKind.Local).AddTicks(9768),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2023, 5, 23, 6, 15, 41, 29, DateTimeKind.Local).AddTicks(2560));
+        }
+    }
+}
